181b4339f0e9b71c4b992f0beb19a968bbf9a2fb
Yuleen <zhuyulin861@qq.com>
2017-01-03 19:37:25 +0800

    um project update

--- provider/src/main/java/um/service/AccountService.java
+++ provider/src/main/java/um/service/AccountService.java
@@ -19,16 +19,19 @@ public class AccountService implements IAccountService {
     public Integer login(String username, String password) throws ServiceException {
         List<AccountDO> checkUserName = accountDAO.findByUserName(username);
         if (checkUserName.size() == 1 && checkUserName.get(1).getPassword().equals(DigestUtils.md5Hex(password))){
-                return checkUserName.get(1).getId();
+            return checkUserName.get(1).getId();
         }
         else if (checkUserName.size() == 0) {
             throw new ServiceException("找不到用户名", "10002");
+
         }
         else if (checkUserName.size() >1) {
             throw new ServiceException("存在多个用户", "10003");
+
         }
         else if  (StringUtils.isBlank(username)) {
             throw new ServiceException("用户名为空", "10001");
+
         }
         else throw new ServiceException("密码错误", "10003");
         }
