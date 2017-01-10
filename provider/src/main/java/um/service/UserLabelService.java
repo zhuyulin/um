03d34628c3f2fe3970a2bfb219b0d8b6d7f39bdc
Yuleen <zhuyulin861@qq.com>
2017-01-10 18:52:10 +0800

    um project update

--- provider/src/main/java/um/service/UserLabelService.java
+++ provider/src/main/java/um/service/UserLabelService.java
@@ -6,6 +6,8 @@ import um.dao.UserLabelDAO;
 import um.dataobject.UserLabelDO;
 import um.vo.UserLabelServiceVO;
 
+import java.util.List;
+
 /**
  * Created by Yuleen on 2017-01-10.
  */
@@ -19,15 +21,17 @@ public class UserLabelService implements IUserLabelService {
         if (userId == 0 || userLabelId == 0) {
             throw new ServiceException("用户名或者标签ID为空", "10007");
         }
-        if (userLabelDAO.checkUserIdAndLabelId(userId, userLabelId).size() != 0) {
+        List<UserLabelDO> list = userLabelDAO.checkUserIdAndLabelId(userId, userLabelId);
+        if (list.size() != 0) {
             throw new ServiceException("该标签已经存在", "10008");
         }
-
-        if (userLabelDAO.checkUserIdAndLabelId(userId, userLabelId).size() == 0) {
+        if (list.size() == 0) {
             userLabelDAO.addUserLabel(userId, userLabelId);
         }
 
-        if (userLabelDAO.checkUserIdAndLabelId(userId, userLabelId).size() != 0){
+        List<UserLabelDO> list2 = userLabelDAO.checkUserIdAndLabelId(userId, userLabelId);
+        if (list2.size() != 0){
+            System.out.println("标签添加成功");
             return true;
         }
         return false;
@@ -38,24 +42,25 @@ public class UserLabelService implements IUserLabelService {
         if (userId == 0 || userLabelId == 0) {
             throw new ServiceException("用户名或者标签ID为空", "10007");
         }
-        if (userLabelDAO.checkUserIdAndLabelId(userId, userLabelId).size() == 0 ) {
+        List<UserLabelDO> list = userLabelDAO.checkUserIdAndLabelId(userId, userLabelId);
+        if (list.size() == 0 ) {
             throw new ServiceException("该标签不存在", "10008");
         }
         else userLabelDAO.deleteUserLabel(userId, userLabelId);
 
-        //todo
-/*        if (userLabelDAO.checkUserIdAndLabelId(userId, userLabelId).get(0).getIsDeleted().equals("Y")){
-
+        //debug
+        List<UserLabelDO> list2 = userLabelDAO.checkUserIdAndLabelId(userId, userLabelId);
+        if (list2.get(0).getIsDeleted().equals("Y")){
             System.out.println("标签已经删除");
             return true;
-        }*/
+        }
         return false;
     }
 
     @Override
     public UserLabelServiceVO getLabel(int userId) throws ServiceException {
             UserLabelDO userLabelDO = (UserLabelDO) userLabelDAO.findByUserId(userId);
-        UserLabelServiceVO userLabelVO = new UserLabelServiceVO();
+            UserLabelServiceVO userLabelVO = new UserLabelServiceVO();
             userLabelVO.setLabelName(userLabelDO.getLabelName());
             userLabelVO.setUserId(userLabelDO.getUserId());
             userLabelVO.setLabelId(userLabelDO.getLabelId());
